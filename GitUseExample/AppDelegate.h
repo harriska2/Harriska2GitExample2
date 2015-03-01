33daf7cc12ec6769d63e71b115276b156aa2cd4c
Kathy Pusztavari <k@kathyandcalvin.com>
2015-03-01 13:43:13 -0800

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Timothy Harris on 3/1/15.
+//  Copyright (c) 2015 Timothy Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Timothy Harris on 3/1/15.
+//  Copyright (c) 2015 Timothy Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
